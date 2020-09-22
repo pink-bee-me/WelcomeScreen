89cc6cf6f9121272a554ada109284f0e5f0983ee
LaurieSue <pink.bee.me@outlook.com>
2020-09-22 11:40:15 -0500

    Add project files.

---
+++ WelcomeScreen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WelcomeScreen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemeber, we're accounting on you!");
+            Console.ReadLine();
+        }
+    }
+}
